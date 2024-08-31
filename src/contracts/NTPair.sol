a633b58485e7b00bf8c7a7b96e8b8f76aa4af0f1
Panayiotis Savva <psavva@gmail.com>
2024-08-31 21:48:06 +0300

    Update NTPair.sol

--- src/contracts/NTPair.sol
+++ src/contracts/NTPair.sol
@@ -1,17 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import "./interfaces/IERC20.sol";
-import "./interfaces/IERC721.sol";
-import "./interfaces/INTPair.sol";
-import "./interfaces/IERC721Receiver.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract NTPair is INTPair, IERC721Receiver {
-    // before deployment change double quoto to one quoto
-    bytes4 public immutable ERC20Transfer =
-        bytes4(keccak256(bytes("transferFrom(address,address,uint256)")));
-    bytes4 public immutable ERC721SafeTransferFrom =
-        bytes4(keccak256(bytes("safeTransferFrom(address,address,uint256)")));
+contract NTPair is IERC721Receiver, ReentrancyGuard {
+    // Constants for ERC20 and ERC721 transfer methods
+    bytes4 private constant ERC20_TRANSFER_FROM_SELECTOR = bytes4(keccak256("transferFrom(address,address,uint256)"));
+    bytes4 private constant ERC721_SAFE_TRANSFER_FROM_SELECTOR = bytes4(keccak256("safeTransferFrom(address,address,uint256)"));
 
     struct Pair {
         address ERC721ContractAddress;
@@ -21,53 +19,24 @@ contract NTPair is INTPair, IERC721Receiver {
         uint256 ERC20SettedTokenValue;
     }
 
-    mapping(address ERC721ContractAddress => mapping(uint256 ERC721TokenId => address ERC20ContractAddress))
-        public ERC721ContractAddress_To_ERC721TokenId_To_ERC20ContractAddress;
-    mapping(address ERC721ContractAddress => mapping(uint256 ERC721TokenId => mapping(address ERC20ContractAddress => address pair)))
-        public ERC721ContractAddress_To_ERC721TokenId_To_ERC20ContractAddress_To_Pair;
-    mapping(address pair => Pair) public PairAddress_To_PairInfo;
+    // Mappings for storing pairs and associations
+    mapping(bytes32 => Pair) public pairs; // Maps a unique ID to each Pair
+    mapping(address => mapping(uint256 => address)) public ERC721ToERC20; // Maps ERC721 token to its paired ERC20 address
+
+    event TokenReceived(address operator, address from, uint256 tokenId, bytes data);
+    event TokenRetrieved(address owner, address ERC721ContractAddress, uint256 ERC721TokenId);
+    event PairCreated(address ERC721ContractAddress, uint256 ERC721TokenId, address ERC20ContractAddress, uint256 ERC20TokenValue, address pair);
+    event Swapped(address ERC721ContractAddress, address ERC20ContractAddress, address from, address to, uint256 ERC721TokenId, uint256 ERC20TokenValue);
+    event ERC20TokenPriceChanged(address ERC721ContractAddress, address ERC20ContractAddress, uint256 ERC721TokenId, uint256 oldPrice, uint256 newPrice);
 
     function onERC721Received(
         address operator,
         address from,
         uint256 tokenId,
         bytes calldata data
-    ) external returns (bytes4) {
+    ) external override returns (bytes4) {
         emit TokenReceived(operator, from, tokenId, data);
         return this.onERC721Received.selector;
-    }
-
-    function retrieveERC721Token(
-        address _ERC721ContractAddress,
-        uint256 _ERC721TokenId
-    ) public {
-        address _ERC20ContractAddress = ERC721ContractAddress_To_ERC721TokenId_To_ERC20ContractAddress[
-                _ERC721ContractAddress
-            ][_ERC721TokenId];
-        address pairAddress = ERC721ContractAddress_To_ERC721TokenId_To_ERC20ContractAddress_To_Pair[
-                _ERC721ContractAddress
-            ][_ERC721TokenId][_ERC20ContractAddress];
-        Pair memory pairInfo = PairAddress_To_PairInfo[pairAddress];
-        require(
-            pairInfo.ERC721TokenOwner == msg.sender,
-            "Not the owner of ERC721 Token"
-        );
-        /*                  STATE CHANGES                   */
-        ERC721ContractAddress_To_ERC721TokenId_To_ERC20ContractAddress_To_Pair[
-            _ERC721ContractAddress
-        ][_ERC721TokenId][_ERC20ContractAddress] = address(0);
-        /*                      EVENT                       */
-        emit TokenRetrieved(
-            pairInfo.ERC721TokenOwner,
-            _ERC721ContractAddress,
-            _ERC721TokenId
-        );
-        /*                     EXCHANGE                     */
-        ERC721TokenTransfer(
-            pairInfo.ERC721TokenOwner,
-            _ERC721ContractAddress,
-            _ERC721TokenId
-        );
     }
 
     function createPair(
@@ -75,186 +44,95 @@ contract NTPair is INTPair, IERC721Receiver {
         address _ERC20ContractAddress,
         uint256 _ERC721TokenId,
         uint256 _ERC20TokenValue
-    ) public {
-        require(
-            ERC721ContractAddress_To_ERC721TokenId_To_ERC20ContractAddress_To_Pair[
-                _ERC721ContractAddress
-            ][_ERC721TokenId][_ERC20ContractAddress] == address(0),
-            "Pair already exists"
-        );
-        require(
-            IERC721(_ERC721ContractAddress).ownerOf(_ERC721TokenId) ==
-                address(this),
-            "Token hasn't been sent to this contract"
-        );
-        // requirement for sended erc721token is in fact a erc721 contract address
-        /* require(supportsInterface(type(IERC721).interfaceId)); */
-        // pair creation
-        bytes32 salt = keccak256(
-            abi.encodePacked(_ERC721ContractAddress, _ERC721TokenId)
-        );
-        address pair = address(uint160(uint256(salt)));
-        // on-contract data
-        ERC721ContractAddress_To_ERC721TokenId_To_ERC20ContractAddress[
-            _ERC721ContractAddress
-        ][_ERC721TokenId] = _ERC20ContractAddress;
-        ERC721ContractAddress_To_ERC721TokenId_To_ERC20ContractAddress_To_Pair[
-            _ERC721ContractAddress
-        ][_ERC721TokenId][_ERC20ContractAddress] = pair;
-        PairAddress_To_PairInfo[pair] = Pair({
+    ) external nonReentrant {
+        bytes32 pairId = keccak256(abi.encodePacked(_ERC721ContractAddress, _ERC721TokenId, _ERC20ContractAddress));
+        require(pairs[pairId].ERC721ContractAddress == address(0), "Pair already exists");
+        require(IERC721(_ERC721ContractAddress).ownerOf(_ERC721TokenId) == address(this), "Token hasn't been sent to this contract");
+
+        pairs[pairId] = Pair({
             ERC721ContractAddress: _ERC721ContractAddress,
             ERC20ContractAddress: _ERC20ContractAddress,
             ERC721TokenOwner: msg.sender,
             ERC721TokenId: _ERC721TokenId,
             ERC20SettedTokenValue: _ERC20TokenValue
         });
-        emit PairCreated(
-            _ERC721ContractAddress,
-            _ERC721TokenId,
-            _ERC20ContractAddress,
-            _ERC20TokenValue,
-            pair
-        );
+
+        ERC721ToERC20[_ERC721ContractAddress][_ERC721TokenId] = _ERC20ContractAddress;
+
+        emit PairCreated(_ERC721ContractAddress, _ERC721TokenId, _ERC20ContractAddress, _ERC20TokenValue, address(this));
+    }
+
+    function retrieveERC721Token(
+        address _ERC721ContractAddress,
+        uint256 _ERC721TokenId
+    ) external nonReentrant {
+        address _ERC20ContractAddress = ERC721ToERC20[_ERC721ContractAddress][_ERC721TokenId];
+        bytes32 pairId = keccak256(abi.encodePacked(_ERC721ContractAddress, _ERC721TokenId, _ERC20ContractAddress));
+        Pair memory pairInfo = pairs[pairId];
+
+        require(pairInfo.ERC721TokenOwner == msg.sender, "Not the owner of ERC721 Token");
+
+        delete pairs[pairId];
+        delete ERC721ToERC20[_ERC721ContractAddress][_ERC721TokenId];
+
+        emit TokenRetrieved(pairInfo.ERC721TokenOwner, _ERC721ContractAddress, _ERC721TokenId);
+        _safeTransferERC721(pairInfo.ERC721ContractAddress, pairInfo.ERC721TokenOwner, pairInfo.ERC721TokenId);
     }
 
     function swap(
         address _ERC721ContractAddress,
         uint256 _ERC721TokenId
-    ) public {
-        /*             VARIBALES TO GO TO PAIR              */
-        address _ERC20ContractAddress = ERC721ContractAddress_To_ERC721TokenId_To_ERC20ContractAddress[
-                _ERC721ContractAddress
-            ][_ERC721TokenId];
-        address pair = ERC721ContractAddress_To_ERC721TokenId_To_ERC20ContractAddress_To_Pair[
-                _ERC721ContractAddress
-            ][_ERC721TokenId][_ERC20ContractAddress];
-        /*                 PAIR REQUIREMENT                 */
-        require(pair != address(0), "Pair hasnt been created");
-        /*                    PAIR INFO                     */
-        Pair memory pairInfo = PairAddress_To_PairInfo[pair];
-        /*           AFTER PRICE BEEN DETERMINED            */
-        /*    REQUIREMENT FOR IF THE CONTRACT ALLOWED       */
-        require(
-            pairInfo.ERC20SettedTokenValue ==
-                IERC20(_ERC20ContractAddress).allowance(
-                    msg.sender,
-                    address(this)
-                ),
-            "Contract hasnt been allowed to make this transfer on erc20 owner behalf"
-        );
-        /*         STATE CHANGING BEFORE TRANSFER           */
-        ERC721ContractAddress_To_ERC721TokenId_To_ERC20ContractAddress_To_Pair[
-            _ERC721ContractAddress
-        ][_ERC721TokenId][_ERC20ContractAddress] = address(0);
-        /*                    EXCHANGE                      */
-        emit Swapped(
-            _ERC721ContractAddress,
-            _ERC20ContractAddress,
-            msg.sender,
-            pairInfo.ERC721TokenOwner,
-            _ERC721TokenId,
-            pairInfo.ERC20SettedTokenValue
-        );
-        ERC20TokenTransfer(
-            msg.sender,
-            pairInfo.ERC721TokenOwner,
-            pairInfo.ERC20ContractAddress,
-            pairInfo.ERC20SettedTokenValue
-        );
-        ERC721TokenTransfer(
-            msg.sender,
-            _ERC721ContractAddress,
-            pairInfo.ERC721TokenId
-        );
-    }
+    ) external nonReentrant {
+        address _ERC20ContractAddress = ERC721ToERC20[_ERC721ContractAddress][_ERC721TokenId];
+        bytes32 pairId = keccak256(abi.encodePacked(_ERC721ContractAddress, _ERC721TokenId, _ERC20ContractAddress));
+        Pair memory pairInfo = pairs[pairId];
 
-    function ERC721TokenTransfer(
-        address to,
-        address _ERC721ContractAddress,
-        uint256 _ERC721TokenId
-    ) private {
-        (bool ERC721Success, bytes memory ERC721Data) = _ERC721ContractAddress
-            .call(
-                abi.encodeWithSelector(
-                    ERC721SafeTransferFrom,
-                    address(this),
-                    to,
-                    _ERC721TokenId
-                )
-            );
+        require(pairInfo.ERC721ContractAddress != address(0), "Pair hasn't been created");
         require(
-            ERC721Success &&
-                (ERC721Data.length == 0 || abi.decode(ERC721Data, (bool))),
-            "NT: ERC721 TRANSFER FAILED"
+            pairInfo.ERC20SettedTokenValue == IERC20(_ERC20ContractAddress).allowance(msg.sender, address(this)),
+            "Contract hasn't been allowed to make this transfer on ERC20 owner's behalf"
         );
-    }
 
-    function ERC20TokenTransfer(
-        address from,
-        address to,
-        address _ERC20ContractAddress,
-        uint256 amount
-    ) private {
-        (bool ERC20Success, bytes memory ERC20Data) = _ERC20ContractAddress
-            .call(abi.encodeWithSelector(ERC20Transfer, from, to, amount));
-        require(
-            ERC20Success &&
-                (ERC20Data.length == 0 || abi.decode(ERC20Data, (bool))),
-            "NT: ERC20 TRAN SFER FAILED"
-        );
+        delete pairs[pairId];
+        delete ERC721ToERC20[_ERC721ContractAddress][_ERC721TokenId];
+
+        emit Swapped(_ERC721ContractAddress, _ERC20ContractAddress, msg.sender, pairInfo.ERC721TokenOwner, _ERC721TokenId, pairInfo.ERC20SettedTokenValue);
+        _safeTransferERC20(msg.sender, pairInfo.ERC721TokenOwner, pairInfo.ERC20ContractAddress, pairInfo.ERC20SettedTokenValue);
+        _safeTransferERC721(pairInfo.ERC721ContractAddress, msg.sender, pairInfo.ERC721TokenId);
     }
 
     function changeERC20TokenPrice(
         address _ERC721ContractAddress,
         uint256 _ERC721TokenId,
         uint256 _newERC20TokenPrice
-    ) public {
-        /*             VARIBALES TO GO TO PAIR              */
-        address _ERC20ContractAddress = ERC721ContractAddress_To_ERC721TokenId_To_ERC20ContractAddress[
-                _ERC721ContractAddress
-            ][_ERC721TokenId];
-        address pair = ERC721ContractAddress_To_ERC721TokenId_To_ERC20ContractAddress_To_Pair[
-                _ERC721ContractAddress
-            ][_ERC721TokenId][_ERC20ContractAddress];
-        /*                 PAIR REQUIREMENT                 */
-        require(pair != address(0), "Pair hasnt been created");
-        /*                    PAIR INFO                     */
-        Pair memory pairInfo = PairAddress_To_PairInfo[pair];
-        /*              OWNERSHIP REQUIREMENT               */
-        require(
-            pairInfo.ERC721TokenOwner == msg.sender,
-            "Not the owner of erc721 token"
-        );
-        /*                 STATE CHANGING                   */
-        PairAddress_To_PairInfo[pair]
-            .ERC20SettedTokenValue = _newERC20TokenPrice;
-        emit ERC20TokenPriceChanged(
-            _ERC721ContractAddress,
-            _ERC20ContractAddress,
-            _ERC721TokenId,
-            pairInfo.ERC20SettedTokenValue,
-            _newERC20TokenPrice
-        );
+    ) external nonReentrant {
+        address _ERC20ContractAddress = ERC721ToERC20[_ERC721ContractAddress][_ERC721TokenId];
+        bytes32 pairId = keccak256(abi.encodePacked(_ERC721ContractAddress, _ERC721TokenId, _ERC20ContractAddress));
+        Pair storage pairInfo = pairs[pairId];
+
+        require(pairInfo.ERC721ContractAddress != address(0), "Pair hasn't been created");
+        require(pairInfo.ERC721TokenOwner == msg.sender, "Not the owner of ERC721 token");
+
+        uint256 oldPrice = pairInfo.ERC20SettedTokenValue;
+        pairInfo.ERC20SettedTokenValue = _newERC20TokenPrice;
+
+        emit ERC20TokenPriceChanged(_ERC721ContractAddress, _ERC20ContractAddress, _ERC721TokenId, oldPrice, _newERC20TokenPrice);
     }
 
-    function returnERC20Token(
+    function _safeTransferERC721(
         address _ERC721ContractAddress,
+        address to,
         uint256 _ERC721TokenId
-    ) public view returns (address) {
-        return
-            ERC721ContractAddress_To_ERC721TokenId_To_ERC20ContractAddress[
-                _ERC721ContractAddress
-            ][_ERC721TokenId];
+    ) private {
+        IERC721(_ERC721ContractAddress).safeTransferFrom(address(this), to, _ERC721TokenId);
     }
 
-    function returnPairAddress(
-        address _ERC721ContractAddress,
+    function _safeTransferERC20(
+        address from,
+        address to,
         address _ERC20ContractAddress,
-        uint256 _ERC721TokenId
-    ) public view returns (address) {
-        return
-            ERC721ContractAddress_To_ERC721TokenId_To_ERC20ContractAddress_To_Pair[
-                _ERC721ContractAddress
-            ][_ERC721TokenId][_ERC20ContractAddress];
+        uint256 amount
+    ) private {
+        IERC20(_ERC20ContractAddress).transferFrom(from, to, amount);
     }
 }
